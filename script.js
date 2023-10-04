76f565f849a963c9614f8278c3afaa051fa24980
Sihle Zondo <140248117+SihleZondo23@users.noreply.github.com>
2023-10-04 23:39:03 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+       // Generate a random number between 1 and 100
+       const targetNumber = Math.floor(Math.random() * 100) + 1;
+       let attempts = 0;
+
+       const message = document.getElementById("message");
+       const inputBox = document.getElementById("inputBox");
+       const submitButton = document.getElementById("submit");
+
+       submitButton.addEventListener("click", function () {
+           const userGuess = parseInt(inputBox.value);
+
+           if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+               message.textContent = "Please enter a valid number between 1 and 100.";
+           } else {
+               attempts++;
+               if (userGuess === targetNumber) {
+                   message.textContent = `Congratulations! You guessed the number ${targetNumber} in ${attempts} attempts.`;
+                   submitButton.disabled = true;
+               } else if (userGuess < targetNumber) {
+                   message.textContent = "Try a higher number.";
+               } else {
+                   message.textContent = "Try a lower number.";
+               }
+           }
+
+           inputBox.value = "";
+           inputBox.focus();
+       });
